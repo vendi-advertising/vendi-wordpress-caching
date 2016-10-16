827686f3075d827cd5406440c19d05237c4b0bbd
chris@vendiadvertising.com <chris@vendiadvertising.com>
2016-10-16 18:36:44 -0500

    Remove unused autoloader and dead comments

--- vendi-wordfence.php
+++ vendi-wordfence.php
@@ -23,9 +23,7 @@ foreach ($wp_plugin_paths as $dir => $realdir) {
     }
 }
 if (!defined('WORDFENCE_FCPATH')) {
-    /** @noinspection PhpConstantReassignmentInspection */
     define('WORDFENCE_FCPATH', __FILE__);
-    /** @noinspection PhpConstantReassignmentInspection */
     define('WORDFENCE_PATH', trailingslashit(dirname(WORDFENCE_FCPATH)));
 }
 
@@ -48,41 +46,3 @@ if(! defined('WORDFENCE_VERSIONONLY_MODE')){ //Used to get version from file.
     require_once 'lib/wfSchema.php';
     \Vendi\Wordfence\Caching\wordfence::install_actions();
 }
-
-// spl_autoload_register(
-//                         function ( $class )
-//                         {
-                            // //PSR-4 compliant autoloader
-                            // //See http://www.php-fig.org/psr/psr-4/
-                            // $prefixes = array(
-                            //                     'Vendi\\HealthTradition\\Click4Rates\\' => VENDI_HEALTHTRADITION_CLICK4RATES_PATH . '/lib/Vendi/HealthTradition/Click4Rates/',
-                            //                 );
-                            
-                            // dump( $class );
-
-                            // foreach( $prefixes as $prefix => $base_dir )
-                            // {
-                            //     // does the class use the namespace prefix?
-                            //     $len = strlen( $prefix );
-                            //     if ( 0 !== strncmp( $prefix, $class, $len ) )
-                            //     {
-                            //         // no, move to the next registered prefix
-                            //         continue;
-                            //     }
-
-                            //     // get the relative class name
-                            //     $relative_class = substr( $class, $len );
-
-                            //     // replace the namespace prefix with the base directory, replace namespace
-                            //     // separators with directory separators in the relative class name, append
-                            //     // with .php
-                            //     $file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
-
-                            //     // if the file exists, require it
-                            //     if ( file_exists( $file ) )
-                            //     {
-                            //         require_once $file;
-                            //     }
-                            // }
-                    //     }
-                    // );
