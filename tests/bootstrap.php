edc889d2b0b0c5c2ccab6f2f78a9967aa67f028e
chris@vendiadvertising.com <chris@vendiadvertising.com>
2016-10-18 17:39:26 -0500

    Renamed plugin in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/vendi-wordfence.php';
+	require dirname( dirname( __FILE__ ) ) . '/vendi-wordpress-caching.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
