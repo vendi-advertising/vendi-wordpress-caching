48f6a3b1b09f9f7dce86791744583b9e88a49b4b
chris@vendiadvertising.com <chris@vendiadvertising.com>
2016-10-18 14:26:43 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../
 rm -f vendi-wordpress-caching.zip
-zip -r9 vendi-wordpress-caching.zip vendi-wordpress-caching/  -x "*/.*" -x *.git* -x tests/* -x bin/* -x .travis.yml -x .editorconfig -x .distignore -x phpunit.xml -x Gruntfile.js -x build.sh -x run-phpunit.sh -x phpunit.xml.dist -x package.json
+zip -r9 vendi-wordpress-caching.zip vendi-wordpress-caching/  -x "*/.*" -x vendi-wordpress-caching/.git* -x vendi-wordpress-caching/tests/* -x vendi-wordpress-caching/bin/* -x vendi-wordpress-caching/.travis.yml -x vendi-wordpress-caching/.editorconfig -x vendi-wordpress-caching/.distignore -x vendi-wordpress-caching/phpunit.xml -x vendi-wordpress-caching/Gruntfile.js -x vendi-wordpress-caching/build.sh -x vendi-wordpress-caching/run-phpunit.sh -x vendi-wordpress-caching/phpunit.xml.dist -x vendi-wordpress-caching/package.json
 cd vendi-wordpress-caching
