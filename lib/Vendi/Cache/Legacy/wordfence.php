33ef9fda49caeb41d06665c4f49d44b48103ed3c
chris@vendiadvertising.com <chris@vendiadvertising.com>
2016-10-23 06:37:56 -0500

    More translatable

--- lib/Vendi/Cache/Legacy/wordfence.php
+++ lib/Vendi/Cache/Legacy/wordfence.php
@@ -514,6 +514,7 @@ class wordfence
 
     private static function setupAdminVars()
     {
+        //Translators... I'm sorry.
         $nonce = wp_create_nonce( 'wp-ajax' );
         wp_localize_script(
                             'vendi-cache-admin',
@@ -531,44 +532,41 @@ class wordfence
                                                             'msg_heading_cache_exclusions' => esc_html__( 'Cache Exclusions', 'Vendi Cache' ),
                                                             'msg_heading_manual_update' => esc_html__( 'You need to manually update your .htaccess', 'Vendi Cache' ),
 
-                                                            'msg_switch_apache' => 'Disk-based cache modifies your website configuration file which is called your .htaccess file.
-                                                                                    To enable disk-based cache we ask that you make a backup of this file.
-                                                                                    This is a safety precaution in case for some reason disk-based Cache is not compatible with your site.<br /><br />
+                                                            'msg_switch_apache' => esc_html__( 'Disk-based cache modifies your website configuration file which is called your .htaccess file. To enable disk-based cache we ask that you make a backup of this file. This is a safety precaution in case for some reason disk-based cache is not compatible with your site.', 'Vendi Cache' ) .
+                                                                                    '<br /><br />
                                                                                     <a
                                                                                        href="' . admin_url( 'admin-ajax.php' ) . '?action=vendi_cache_downloadHtaccess&amp;nonce=' . $nonce . '"
-                                                                                       onclick="jQuery(\'#wfNextBut\').prop(\'disabled\', false); return true;">Click here to download a backup copy of your .htaccess file now</a><br /><br />
-                                                                                    <input type="button" name="but1" id="wfNextBut" value="Click to Enable Disk-based cache Engine" disabled="disabled" onclick="WFAD.confirmSwitchToFalcon(0);" />
+                                                                                       onclick="jQuery(\'#wfNextBut\').prop(\'disabled\', false); return true;">' . esc_html__( 'Click here to download a backup copy of your .htaccess file now', 'Vendi Cache' ) . '</a><br /><br />
+                                                                                    <input type="button" name="but1" id="wfNextBut" value="' . esc_attr_x( 'Click to Enable Disk-based cache Engine' ,'Vendi Cache' ) . '" disabled="disabled" onclick="WFAD.confirmSwitchToFalcon(0);" />
                                                                                     ',
-                                                            'msg_switch_nginx'  => '
-                                                                                    You are using an Nginx web server and using a FastCGI processor like PHP5-FPM.
-                                                                                    To use disk-based cache you will need to manually modify your nginx.conf configuration file and reload your Nginx server for the changes to take effect.
-                                                                                    You can find the <a href="http://www.wordfence.com/blog/2014/05/nginx-wordfence-falcon-engine-php-fpm-fastcgi-fast-cgi/" target="_blank">rules you need to make these changes to nginx.conf on this page on wordfence.com</a>.
-                                                                                    Once you have made these changes, compressed cached files will be served to your visitors directly from Nginx making your site extremely fast.
-                                                                                    When you have made the changes and reloaded your Nginx server, you can click the button below to enable Disk-based cache.<br /><br />
-                                                                                    <input type="button" name="but1" id="wfNextBut" value="Click to Enable Disk-based cache Engine" onclick="WFAD.confirmSwitchToFalcon(1);" />
+                                                            'msg_switch_nginx'  => sprintf(
+                                                                                            wp_kses(
+                                                                                                        __( 'You are using an Nginx web server and using a FastCGI processor like PHP5-FPM. To use disk-based cache you will need to manually modify your nginx.conf configuration file and reload your Nginx server for the changes to take effect. You can find the <a href="%s" target="_blank">rules you need to make these changes to nginx.conf on this page on wordfence.com</a>. Once you have made these changes, compressed cached files will be served to your visitors directly from Nginx making your site extremely fast. When you have made the changes and reloaded your Nginx server, you can click the button below to enable Disk-based cache.', 'Vendi Cache' ),
+                                                                                                        array(
+                                                                                                                'a' => array(
+                                                                                                                                'href' => array(),
+                                                                                                                                'target' => array(),
+                                                                                                                            ),
+                                                                                                        )
+                                                                                                ),
+                                                                                            esc_url( 'http://www.wordfence.com/blog/2014/05/nginx-wordfence-falcon-engine-php-fpm-fastcgi-fast-cgi/' )
+                                                                                            ) .
+                                                                                    '<br /><br /><input type="button" name="but1" id="wfNextBut" value="' . esc_attr_x( 'Click to Enable Disk-based cache Engine' ,'Vendi Cache' ) . '" onclick="WFAD.confirmSwitchToFalcon(1);" />',
+
+                                                            'msg_switch_error'  => esc_html__( 'We can\'t modify your .htaccess file for you because: @@1@@', 'Vendi Cache' ) .
+                                                                                    '<br /><br />' .
+                                                                                    esc_html__( 'Advanced users: If you would like to manually enable disk-based cache yourself by editing your .htaccess, you can add the rules below to the beginning of your .htaccess file. Then click the button below to enable disk-based cache. Don\'t do this unless you understand website configuration.', 'Vendi Cache' ) .
+                                                                                    '<br /><textarea style="width: 300px; height:100px;" readonly>@@2@@</textarea><br />
+                                                                                    <input type="button" value="' . esc_attr_x( 'Enable disk-based cache after manually editing .htaccess', 'Vendi Cache' ) . '" onclick="WFAD.confirmSwitchToFalcon(1);" />
+                                                                                ',
+                                                            'msg_manual_update' => '@@1@@<br />' . 
+                                                                                    esc_html__( 'Your option was updated but you need to change the disk-base cache code in your .htaccess to the following:', 'Vendi Cache' ) .
+                                                                                    '<br /><textarea style="width: 300px; height: 120px;">@@2@@</textarea>
                                                                                 ',
 
-                                                            'msg_switch_error'  => '
-                                                                                    We can\'t modify your .htaccess file for you because: @@1@@<br /><br />
-                                                                                    Advanced users: If you would like to manually enable disk-based cache yourself by editing your .htaccess, you can add the rules below to the beginning of your .htaccess file.
-                                                                                    Then click the button below to enable disk-based cache. Don\'t do this unless you understand website configuration.<br />
-                                                                                    <textarea style="width: 300px; height:100px;" readonly>@@2@@</textarea><br />
-                                                                                    <input type="button" value="Enable disk-based cache after manually editing .htaccess" onclick="WFAD.confirmSwitchToFalcon(1);" />
-                                                                                ',
-                                                            'msg_manual_update' => '
-                                                                                    @@1@@<br />
-                                                                                    Your option was updated but you need to change the Wordfence code in your .htaccess to the following:<br />
-                                                                                    <textarea style="width: 300px; height: 120px;">@@2@@</textarea>
-                                                                                ',
+                                                            'msg_invalid_pattern' => esc_html__( 'You can not enter full URL\'s for exclusion from caching. You entered a full URL that started with http:// or https://. You must enter relative URL\'s e.g. /exclude/this/page/. You can also enter text that might be contained in the path part of a URL or at the end of the path part of a URL.', 'Vendi Cache' ) ,
 
-                                                            'msg_invalid_pattern' => '
-                                                                                        You can not enter full URL\'s for exclusion from caching.
-                                                                                        You entered a full URL that started with http:// or https://.
-                                                                                        You must enter relative URL\'s e.g. /exclude/this/page/.
-                                                                                        You can also enter text that might be contained in the path part of a URL or at the end of the path part of a URL.
-                                                                                ',
-
-                                                            'msg_no_exclusions' => __( 'There are not currently any exclusions. If you have a site that does not change often, it is perfectly normal to not have any pages you want to exclude from the cache.', 'Vendi Cache' ),
+                                                            'msg_no_exclusions' => esc_html__( 'There are not currently any exclusions. If you have a site that does not change often, it is perfectly normal to not have any pages you want to exclude from the cache.', 'Vendi Cache' ),
                                                         )
                         );
     }
@@ -582,7 +580,7 @@ class wordfence
         }
         if( $activationError )
         {
-            echo '<div id="wordfenceConfigWarning" class="updated fade"><p><strong>Wordfence generated an error on activation. The output we received during activation was:</strong> ' . wp_kses( $activationError, array() ) . '</p></div>';
+            echo '<div class="updated fade"><p><strong>' . esc_html__( 'Vendi Cache generated an error on activation. The output we received during activation was:', 'Vendi Cache' ) . '</strong> ' . wp_kses( $activationError, array() ) . '</p></div>';
         }
         delete_option( 'wf_plugin_act_error' );
     }
