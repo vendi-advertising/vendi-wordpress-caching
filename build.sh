8912b286c47cd7d7f1e75abe36a290d81d6354c2
chris@vendiadvertising.com <chris@vendiadvertising.com>
2016-11-12 14:35:10 -0600

    Build script now updates language file

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ fi
 
 ./bin/wp2md convert < readme.txt > README.md
 
+php /home/chrishaas/wordpress-i18n-tools/i18n/makepot.php wp-plugin . languages/vendi-cache.pot
+
 cd ../
 rm -f vendi-cache.zip
 zip -r9 vendi-cache.zip vendi-cache/  -x "*/.*" -x vendi-cache/.git* -x "vendi-cache/tests*" -x "vendi-cache/bin*" -x vendi-cache/.travis.yml -x vendi-cache/*.md -x vendi-cache/.editorconfig -x vendi-cache/.distignore -x vendi-cache/phpunit.xml -x vendi-cache/Gruntfile.js -x vendi-cache/build.sh -x vendi-cache/run-phpunit.sh -x vendi-cache/phpunit.xml.dist -x vendi-cache/package.json
