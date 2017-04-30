f6fb00f2859b8064f4a78245d80026efce1f38c9
chris@vendiadvertising.com <chris@vendiadvertising.com>
2017-04-30 15:10:13 -0500

    Add cache exclusion

--- lib/Vendi/Cache/AjaxCallbacks/add_cache_exclusion.php
+++ lib/Vendi/Cache/AjaxCallbacks/add_cache_exclusion.php
@@ -2,6 +2,7 @@
 
 namespace Vendi\Cache\AjaxCallbacks;
 
+use Vendi\Cache\cache_setting_exception;
 use Vendi\Cache\cache_settings;
 use Vendi\Cache\ajax_message;
 use Vendi\Cache\ajax_error;
@@ -21,12 +22,15 @@ class add_cache_exclusion extends ajax_callback_base
 
         self::get_vwc_cache_settings()->set_cache_exclusions( $ex );
         wfCache::schedule_cache_clear();
-        if( self::get_vwc_cache_settings()->get_cache_mode() == cache_settings::CACHE_MODE_ENHANCED && preg_match( '/^(?:uac|uaeq|cc)$/', utils::get_post_value( 'patternType' ) ) )
+        if( self::get_vwc_cache_settings()->get_cache_mode() == cache_settings::CACHE_MODE_ENHANCED ) )
         {
-            //rewrites htaccess rules
-            if( wfCache::add_htaccess_code( 'add' ) )
+            if( in_array( utils::get_post_value( 'patternType' ), array( cache_setting_exception::USER_AGENT_CONTAINS, cache_setting_exception::USER_AGENT_MATCHES_EXACTLY, cache_setting_exception::COOKIE_NAME_CONTAINS ) ) )
             {
-                return new ajax_error( esc_html__( 'We added the rule you requested but could not modify your .htaccess file. Please delete this rule, check the permissions on your .htaccess file and then try again.', 'Vendi Cache' ) );
+                //rewrites htaccess rules
+                if( wfCache::add_htaccess_code( 'add' ) )
+                {
+                    return new ajax_error( esc_html__( 'We added the rule you requested but could not modify your .htaccess file. Please delete this rule, check the permissions on your .htaccess file and then try again.', 'Vendi Cache' ) );
+                }
             }
         }
         return new ajax_message();
