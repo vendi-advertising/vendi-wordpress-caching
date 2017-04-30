85ac9fb8158b88ef64c075295bac81a7a11bdc5c
chris@vendiadvertising.com <chris@vendiadvertising.com>
2017-04-30 16:09:09 -0500

    Removed extra parentheses

--- lib/Vendi/Cache/AjaxCallbacks/add_cache_exclusion.php
+++ lib/Vendi/Cache/AjaxCallbacks/add_cache_exclusion.php
@@ -22,7 +22,7 @@ class add_cache_exclusion extends ajax_callback_base
 
         self::get_vwc_cache_settings()->set_cache_exclusions( $ex );
         wfCache::schedule_cache_clear();
-        if( self::get_vwc_cache_settings()->get_cache_mode() == cache_settings::CACHE_MODE_ENHANCED ) )
+        if( self::get_vwc_cache_settings()->get_cache_mode() == cache_settings::CACHE_MODE_ENHANCED )
         {
             if( in_array( utils::get_post_value( 'patternType' ), array( cache_setting_exception::USER_AGENT_CONTAINS, cache_setting_exception::USER_AGENT_MATCHES_EXACTLY, cache_setting_exception::COOKIE_NAME_CONTAINS ) ) )
             {
