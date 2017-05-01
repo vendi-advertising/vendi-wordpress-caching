fc595f08ce3beffa9a3920bc78cb239bb542dfaa
chris@vendiadvertising.com <chris@vendiadvertising.com>
2017-04-30 19:21:05 -0500

    Start using true Exceptions

---
+++ lib/Vendi/Cache/Exceptions/file_system_exception.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vendi\Cache\Exceptions;
+
+
+class file_system_exception extends \Exception
+{
+    private $_error_get_last;
+
+    public function __construct( string $message = null, array $error_get_last = null )
+    {
+        parent::__construct( $message );
+        $this->error_get_last = $error_get_last;
+    }
+
+    public function __toString()
+    {
+        if( $this->error_get_last )
+        {
+            $type    = $this->error_get_last[ 'type' ];
+            $message = $this->error_get_last[ 'message' ];
+            $file    = $this->error_get_last[ 'file' ];
+            $line    = $this->error_get_last[ 'line' ];
+
+            return __CLASS__ . ": [{$this->code}]: [Type: {$type}] [File: {$file}] [Line: {$line}] [Primary: {$this->message}] [Secondary: {$message}]\n";
+        }
+
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
